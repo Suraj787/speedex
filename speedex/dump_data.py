58bc3375db988cf760ce6d5253f69c7190a8746f
tusharpatil3688 <tusharpatil3688@gmail.com>
2020-12-02 12:12:24 +0530

    convert reponse type bytes to string

--- speedex/dump_data.py
+++ speedex/dump_data.py
@@ -8,51 +8,55 @@ def data_entry():
     ref_list = []
     r = requests.get('https://speedexlogistics.com/s/api/accounts/sea/cheque_payment/list.php',
     headers={'sessionID': '1'}) 
-#     print(r.content)
-#     print(type(r.content))
     op=r.content
     if type(r.content)==bytes:
        op=(r.content).decode("utf-8")
     item_list = json.loads(op)
-    print(item_list)
-#     all_pi = frappe.get_all("Purchase Invoice", ["ref_no"])
-#     for pi in all_pi:
-#         ref_list.append(pi.get('ref_no')) 
-#     for item in item_list:
-#         if item.get('ref') not in ref_list:
-#             pi_doc = frappe.get_doc({
-#                 "doctype" : "Purchase Invoice",
-#                 "company" : "Speedex Logistics Limited",
-#                 "supplier" : "Government",
-#                 "client_id" : item.get('client_id'),
-#                 "ref_no" : item.get('ref'),
-#                 "sea_id" : item.get('sea_id'),
+    all_pi = frappe.get_all("Purchase Invoice", ["ref_no"])
+    for pi in all_pi:
+        ref_list.append(pi.get('ref_no')) 
+    for item in item_list:
+        if item.get('ref') not in ref_list:
+            ref_list.append(item.get('ref'))
+            pi_doc = frappe.get_doc({
+                "doctype" : "Purchase Invoice",
+                "company" : "Speedex Logistics Limited",
+                "supplier" : "Government",
+                "client_id" : item.get('client_id'),
+                "ref_no" : item.get('ref'),
+                "sea_id" : item.get('sea_id'),
 
-#             })
-#             for i in item.get('items'):
-#                 for my_item in i.keys():
-#                     pi_doc.append("items",{
-#                         "item_name": my_item,
-#                         "qty" : 1,
-#                         "expense_account" :"Administrative Expenses - SLL" ,
-#                         "uom" : "Nos",
-#                     })
-#             pi_doc.insert()
-#             pi_doc.submit()
-#     return 'OK'
+            })
+            for i in item.get('items'):
+                for my_item in i.keys():
+                    q = "select expense_account from `tabItem Default` where parent ='{0}';".format(my_item)
+                    expense_acc = frappe.db.sql(q)
+                    pi_doc.append("items",{
+                        "item_name": my_item,
+                        "qty" : 1,
+                        "expense_account" :expense_acc[0][0] if expense_acc and expense_acc[0][0] else "Administrative Expenses - SLL" ,
+                        "uom" : "Nos",
+                    })
+            pi_doc.insert()
+            pi_doc.submit()
+    return 'OK'
 
 # bench execute speedex.dump_data.payment_entry
 def payment_entry():
     ref_list = []
     r = requests.get('https://speedexlogistics.com/s/api/accounts/sea/cheque_payment/cheque_payment_entries.php',
     headers={'sessionID': '1'}) 
-    item_list = json.loads(r.content)
+    op=r.content
+    if type(r.content)==bytes:
+       op=(r.content).decode("utf-8")
+    item_list = json.loads(op)
     all_pi = frappe.get_all("Payment Entry", ["ref_no"])
     import datetime
     for pi in all_pi:
         ref_list.append(pi.get('ref')) 
     for item in item_list:
         if item.get('ref') not in ref_list:
+            ref_list.append(item.get('ref'))
             datetime_obj = datetime.datetime.strptime(item.get('date_opened'), '%d/%m/%Y')
             for d in frappe.get_all('Purchase Invoice',{'ref_no':item.get('ref')},['name','supplier','due_date']):
                 pe_doc = frappe.get_doc({
