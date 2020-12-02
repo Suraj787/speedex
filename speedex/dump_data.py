0ddc205e9258fcd7ec2262cffc7627f96e5ff36e
Suraj varade <varade.suraj787@gmail.com>
2020-12-02 11:34:54 +0530

    Update dump_data.py
    debug

--- speedex/dump_data.py
+++ speedex/dump_data.py
@@ -8,32 +8,34 @@ def data_entry():
     ref_list = []
     r = requests.get('https://speedexlogistics.com/s/api/accounts/sea/cheque_payment/list.php',
     headers={'sessionID': '1'}) 
-    item_list = json.loads(r.content)
-    all_pi = frappe.get_all("Purchase Invoice", ["ref_no"])
-    for pi in all_pi:
-        ref_list.append(pi.get('ref_no')) 
-    for item in item_list:
-        if item.get('ref') not in ref_list:
-            pi_doc = frappe.get_doc({
-                "doctype" : "Purchase Invoice",
-                "company" : "Speedex Logistics Limited",
-                "supplier" : "Government",
-                "client_id" : item.get('client_id'),
-                "ref_no" : item.get('ref'),
-                "sea_id" : item.get('sea_id'),
+    print(r.content)
+    print(type(r.content))
+#     item_list = json.loads(r.content)
+#     all_pi = frappe.get_all("Purchase Invoice", ["ref_no"])
+#     for pi in all_pi:
+#         ref_list.append(pi.get('ref_no')) 
+#     for item in item_list:
+#         if item.get('ref') not in ref_list:
+#             pi_doc = frappe.get_doc({
+#                 "doctype" : "Purchase Invoice",
+#                 "company" : "Speedex Logistics Limited",
+#                 "supplier" : "Government",
+#                 "client_id" : item.get('client_id'),
+#                 "ref_no" : item.get('ref'),
+#                 "sea_id" : item.get('sea_id'),
 
-            })
-            for i in item.get('items'):
-                for my_item in i.keys():
-                    pi_doc.append("items",{
-                        "item_name": my_item,
-                        "qty" : 1,
-                        "expense_account" :"Administrative Expenses - SLL" ,
-                        "uom" : "Nos",
-                    })
-            pi_doc.insert()
-            pi_doc.submit()
-    return 'OK'
+#             })
+#             for i in item.get('items'):
+#                 for my_item in i.keys():
+#                     pi_doc.append("items",{
+#                         "item_name": my_item,
+#                         "qty" : 1,
+#                         "expense_account" :"Administrative Expenses - SLL" ,
+#                         "uom" : "Nos",
+#                     })
+#             pi_doc.insert()
+#             pi_doc.submit()
+#     return 'OK'
 
 # bench execute speedex.dump_data.payment_entry
 def payment_entry():
